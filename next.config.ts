ad650aa203f862dcb443f861554ebfceda6eae00
Aditya <adimail2404@gmail.com>
2026-03-30 17:31:27 +0530

    fix: set images.unoptimized to true

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "drive.google.com",
-      },
+      { protocol: "https", hostname: "drive.google.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
